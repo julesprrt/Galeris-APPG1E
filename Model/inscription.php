d73be6f0dd6b8f8a65e9a677031fa83c15b2d9a0
akash47 <65364908+Akashos23@users.noreply.github.com>
2024-10-12 15:04:12 +0200

    Finalisation lien database

--- Model/inscription.php
+++ Model/inscription.php
@@ -1,16 +1,28 @@
 <?php
-
 require_once('../Database/Database.php');
 
-function PostUser(){
-    $sql = "INSERT INTO utilisateur (nom, prenom, email, description, adresse, roles, mot_de_passe, date_creation, newsletter)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+/**
+ * Summary of PostUser
+ * Méthode permettant d'ajouter un utilisateur dans la BD (Fonction de test)
+ * @param Database $database
+ * @return void
+ */
+function PostUser(Database $database){
+    $sql = "INSERT INTO utilisateur (nom, prenom, adresse, roles, mot_de_passe, date_creation, newsletter)
+            VALUES (?, ?, ?, ?, ?, ?, ?)";
     
-    $db = connect();
+    $db = $database->connect();
     $stmt = $db->prepare($sql);
 
+    $nom = 'test';
+    $prenom = 'test';
+    $email = 'test@gmail.com';
+    $roles = 'FALSE';
+    $mot_de_passe = 'test';
+    $date_creation = '2024-02-03';
+    $newsletter = 'TRUE';
 
-    $stmt->bind_param('sssssssss', $nom, $prenom, $email, $description, $adresse, $roles, $mot_de_passe, $date_creation, $newsletter);
+    $stmt->bind_param('sssssss',$nom, $prenom, $email, $roles, $mot_de_passe, $date_creation, $newsletter);
     
-   $stmt->execute();
+    $stmt->execute();
 }
