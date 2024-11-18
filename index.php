c83fb9cbb369bd755ef83b0e2f534517cc92c062
akash47 <65364908+Akashos23@users.noreply.github.com>
2024-11-18 16:42:22 +0100

    Routage terminé

--- index.php
+++ index.php
@@ -19,9 +19,9 @@ $router->addRoute('/Galeris-APPG1E/', HomeController::class, action: 'home');
 $router->addRoute('/Galeris-APPG1E/inscription', UserController::class, action: 'inscription');
 $router->addRoute('/Galeris-APPG1E/connexion', UserController::class, 'connexion');
 $router->addRoute('/Galeris-APPG1E/contact', ContactController::class, 'contact');
-$router->addRoute('/Galeris-APPG1E/FAQ', FAQController::class, 'faq');
+$router->addRoute('/Galeris-APPG1E/faq', FAQController::class, 'faq');
 $router->addRoute('/Galeris-APPG1E/cgu', CGUController::class, 'cgu');
 $router->addRoute('/Galeris-APPG1E/achat', AchatController::class, 'achat');
-
+$router->addRoute('/Galeris-APPG1E/motdepasse', UserController::class, 'password');
 
 $router->dispatch($uri);//Appel a la méthode du controller dedié
