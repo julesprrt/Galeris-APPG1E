8cb63ac571ee1f54ebebc36c0234f6774c6e7a82
YannRM1 <yann.roquigny.martin@gmail.com>
2024-12-13 11:02:35 +0100

    Update EnchereController.php

--- Controller/EnchereController.php
+++ Controller/EnchereController.php
@@ -3,5 +3,14 @@ require_once('Model/enchere.php');
 require_once('Database/Database.php');
 require_once('Controller.php');
 
-JE 
+Class EnchereController extends Controller {
+
+
+    public function oeuvre(){
+        $paramData = file_get_contents("php://input");
+        $data = json_decode($paramData, true);
+    }
+
+
+}
 ?>
