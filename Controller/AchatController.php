2e8c48aa7ab487a4ad3bc58cb4ab28bee06c56a1
YannRM1 <yann.roquigny.martin@gmail.com>
2024-12-15 11:13:24 +0100

    yrm : debut achat back

--- Controller/AchatController.php
+++ Controller/AchatController.php
@@ -1,11 +1,28 @@
 <?php
-require_once('Model/user.php');
+require_once('Model/enchere.php');
 require_once('Database/Database.php');
 require_once('Controller.php');
 Class AchatController extends Controller{//Controlleur accueil
     
-    public function achat() {
-        $this->render('achat', []);
+    public function achat(Database $db) {
+        $id = $_GET['id'] ?? null ;// si on trouve pas id renvoie rien
+        
+        if ($id){
+
+            $artModele = new Art();
+            $oeuvre = $artModele->getArtById($id,$db);
+
+
+            if ($oeuvre){
+                $this->render('achat',['oeuvre'=> $oeuvre]);
+                
+            } else {
+                $this->render('404',['message'=> 'oeuvre introuvable']);
+            
+            }
+
+        }
+
         
     }
 }
