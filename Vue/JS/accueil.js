cd10f4a3dbba37432cc7b149019aa0465bce3891
akash47 <65364908+Akashos23@users.noreply.github.com>
2024-12-16 11:54:12 +0100

    fix error (en cours)

--- Vue/JS/accueil.js
+++ Vue/JS/accueil.js
@@ -1,7 +1,11 @@
-document.getElementById("oeuvre").addEventListener('click', saveId);
+
+const cbox = document.querySelectorAll(".oeuvreOBJ");
+
+ for (let i = 0; i < cbox.length; i++) {
+     cbox[i].addEventListener("click", saveId());
+ }
 
 async function saveId() {
-    console.log("OK");
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
@@ -18,7 +22,7 @@ async function saveId() {
     const response = await fetch("https://galeris/Galeris-APPG1E/saveid", requestOptions)
     const statuscode = response.status;
     if (statuscode === 200) {
-        window.location.href = "https://galeris/Galeris-APPG1E/achat";
+       // window.location.href = "https://galeris/Galeris-APPG1E/achat";
 
     }
     else {
