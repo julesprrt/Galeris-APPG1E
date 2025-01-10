d843094e1a19b40302a6b0529071990e976e3923
Jules <jules.pierret4582@gmail.com>
2025-01-10 15:23:34 +0100

    AMJ

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -382,22 +382,4 @@ class UserController extends Controller
         // Transmet les données utilisateur à la vue
         $this->render('profil_utilisateur', ['user' => $userData, "connectUser" => isset($_SESSION["usersessionID"]), "userRole" => $role]);
     }
-
-
-    public function historique(Database $db)
-    {
-        session_start();
-
-        if (!isset($_SESSION["usersessionID"])) {
-            header('Location: /Galeris-APPG1E/connexion');
-            exit;
-        }
-
-        $historique = new Historique();
-        $historiqueResult = $historique->getAllHistorique($db);
-        $achat = new Historique();
-        $achatResult = $achat->getAllAchat($db);
-        $role = isset($_SESSION["usersessionRole"]) && $_SESSION["usersessionRole"] === "Admin";
-        $this->render('historique', ["connectUser" => isset($_SESSION["usersessionID"]), "userRole" => $role, "historique" => $historiqueResult, "achat" => $achatResult]);
-    }
 }
