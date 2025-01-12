96a85df1b3a40ddedc0b8080ee346a6e20715483
akash47 <65364908+Akashos23@users.noreply.github.com>
2025-01-12 01:02:51 +0100

    fr

--- Vue/html/galeris.php
+++ Vue/html/galeris.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
     <link rel="stylesheet" href="Vue/CSS/galeris.css">
     <link rel="stylesheet" href="Vue/CSS/header.css">
     <link rel="stylesheet" href="Vue/CSS/footer.css">
