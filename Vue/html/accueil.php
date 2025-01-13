df8630d349b9e38881decfbec523c1c3612f2a1d
antoineclz <antoine.caliez@eleve.isep.fr>
2025-01-13 13:11:54 +0100

    repare "accueil"

--- Vue/html/accueil.php
+++ Vue/html/accueil.php
@@ -72,6 +72,56 @@
                 ?>
 
             </div>
+        </div>
+
+        <nav class="menu">
+            <ul>
+                <li><a href="./">Accueil</a></li>
+                <li><a href="./ventes">Vente</a></li>
+                <li><a href="./exposes">Exposition</a></li>
+                <li><a href="./listenews">News</a></li>
+                
+            </ul>
+        </nav>
+        <div class="barre_recherche">
+        <input type="text" placeholder="Rechercher..." class="shearch">
+            <datalist id="galeris-list">
+                        <?php
+                            foreach($users as $user_barre){
+                                echo '<option data-value="utilisateur_' . $user_barre["id_utilisateur"] . '" value="' . $user_barre["nom"] . ' ' . $user_barre["prenom"] . ' ' . $user_barre["id_utilisateur"] .' (utilisateur)">';
+                            }
+                            foreach($exposes as $expose_barre){
+                                echo '<option data-value="expose_' . $expose_barre["id_exhibition"] . '" value="' . $expose_barre["titre"] . ' ' . $expose_barre["id_exhibition"] . ' (exposé)">';
+                            }
+                            foreach($oeuvres as $oeuvre_barre){
+                                echo '<option data-value="oeuvre_' . $oeuvre_barre["id_oeuvre"] . '" value="' . $oeuvre_barre["Titre"] . ' ' . $oeuvre_barre["auteur"] . ' ' . $oeuvre_barre["id_oeuvre"] . ' (Oeuvre)">';
+                            }
+                                
+                        ?>
+            </datalist>
+            <div class="favori"> <a href="./favoris">❤️</a></div>
+            <div class="panier"> <a href="./panier">🛒</a></div>
+
+            <?php
+            if ($connectUser === true) {
+                echo '<div class="dropdown">
+                            <div class="utilisateur"> 👤 </div>
+                            <div class="dropdown-child">
+                                <a href="./profil">Mon profil</a>
+                                <a href="./solde">Mon solde</a>' .
+                    (($userRole === true) ?
+                        '<a href="./listeoeuvreattente">Oeuvres en attente</a>
+                                    <a href="./listeexposeattente">Exposés en attente</a>' : "") .
+                    '<a id="deconnexion">Déconnexion</a>
+                            </div>
+                           </div>';
+            } else {
+                echo '<div class="utilisateur"><a href="./connexion"> 👤 </a></div>';
+            }
+            ?>
+
+
+        </div>
         </header>
 
         <!-- Contenu de la page d'accueil -->
