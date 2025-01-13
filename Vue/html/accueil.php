ef988f5e2790b0a11759ab459e8ed9930e5a297a
akash47 <65364908+Akashos23@users.noreply.github.com>
2025-01-13 17:09:00 +0100

    terminer

--- Vue/html/accueil.php
+++ Vue/html/accueil.php
@@ -52,7 +52,8 @@
                                 <a href="./solde">Mon solde</a>'.
                                 (($userRole === true)?
                                     '<a href="./listeoeuvreattente">Oeuvres en attente</a>
-                                    <a href="./listeexposeattente">Exposés en attente</a>':"").
+                                    <a href="./listeexposeattente">Exposés en attente</a>
+                                    <a href="./dashboard">Tableau de bord</a>' : "") .
                                 '<a id="deconnexion">Déconnexion</a>
                             </div>
                            </div>';
