89331b812cf42e7b667a1b40672501a8847b4789
gabriel-04-10 <156429999+gabriel-04-10@users.noreply.github.com>
2025-01-14 16:18:26 +0100

    .

--- DatabaseToImport/favoris.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE favoris(
-id_favoris INTEGER PRIMARY KEY AUTO_INCREMENT,
-id_utilisateur INTEGER,
-id_oeuvre INTEGER,
-FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur) ON DELETE CASCADE
-FOREIGN KEY (id_oeuvre) REFERENCES oeuvre(id_oeuvre) ON DELETE CASCADE
-);
