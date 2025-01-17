751b3a708edf87ea443323135c7df91d49c63e07
gabriel-04-10 <156429999+gabriel-04-10@users.noreply.github.com>
2025-01-17 12:02:06 +0100

    .

--- Vue/html/listeattentexpose.php
+++ Vue/html/listeattentexpose.php
@@ -54,7 +54,7 @@
                                 <a href="./solde">Mon solde</a>' .
                         (($userRole === true) ?
                             '<a href="./listeoeuvreattente">Oeuvres en attente</a>
-                                    <a href="./listeexposeattente">Exposés en attente</a> 
+                                    <a href="./listeexposeattente">Expositions en attente</a> 
                                     <a href="./dashboard">Tableau de bord</a>' : "") .
                         '<a class="deconnexion">Déconnexion</a>
                             </div>
@@ -71,7 +71,7 @@
         <!-- Contenu de la page d'accueil -->
         <div class="page-content">
             <div class="contentbase">
-            <h2 class="title-expose">Liste des exposés en attente</h2>
+            <h2 class="title-expose">Liste des expositions en attente</h2>
                     <div class="exposes">
                         <?php
                         foreach ($exposes as $expose) {
